510425ef6e3a2a2d3c41adcd9ba90d04f01c0b37
Kamilkampfwagen <kamilkampfwagen@gmail.com>
2023-09-05 10:33:18 +0300

    Launcher script enforces graphics level

--- fullscreen.ps1
+++ fullscreen.ps1
@@ -3,15 +3,18 @@ Put the following line to Steam -> Manage -> Properties -> Launch Options
 Powershell -ExecutionPolicy bypass -Command & ""%command%/../fullscreen.ps1""
 #>
 
+$windowResolution = (1920,1080)
+
 Set-Location $PSScriptRoot
 
 $configPath = "$env:LOCALAPPDATA/Microsoft Games/Chess Titans/ChessTitans.xml"
-
 $config = Get-Content -Path $configPath
+$config[12] = "    <Rendering>2</Rendering>"
+$config[13] = "    <RenderingLast3D>2</RenderingLast3D>"
 $config[17] = "    <WindowX>100</WindowX>"
 $config[18] = "    <WindowY>100</WindowY>"
-$config[19] = "    <WindowWidth>1920</WindowWidth>"
-$config[20] = "    <WindowHeight>1080</WindowHeight>"
-Set-Content -Path $configPath -Value $config
+$config[19] = "    <WindowWidth>$($windowResolution[0])</WindowWidth>"
+$config[20] = "    <WindowHeight>$($windowResolution[1])</WindowHeight>"
+Set-Content -Path $configPath -Value $config -Force
 
-$process = Start-Process "./chess.exe" -WorkingDirectory '.' -PassThru
+Start-Process "./chess.exe" -WorkingDirectory '.' -PassThru
